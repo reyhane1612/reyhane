bad0811629023f7eeecb5e9e6d735813600cf1a6
Reyhane Hassani <ryhane.h.1612@gmail.com>
2025-11-02 23:45:51 +0330

    test3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello Im Reyhane Hassani</h1><p>Welcome to my website!</p>"
+    return "<h1>Hello Im Reyhane Hassani</h1><p>Welcome here</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
